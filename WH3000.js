59e83532c6ac2775e1c3ff84171452cb1afce4ad
Joanne <joannemy@my.yorku.ca>
2022-11-14 12:14:28 -0500

    Username

--- WH3000.js
+++ WH3000.js
@@ -1,7 +1,12 @@
 function submit () {
-    var uname = document.getElementById("uname").value;
     var unameArr = ["hello"];
+    flag = false
+
+
+    var uname = document.getElementById("uname").value;
 
     if (unameArr.includes(uname)== true)
-        document.getElementById("output").innerHTML = "Username already in use, please pick a different username."; 
+        document.getElementById("output").innerHTML = "Username already in use, please pick a different username.";
+    else 
+        document.getElementById("output").innerHTML = "Username is valid, please proceed";
 }

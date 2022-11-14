01170fd80bc804ec57a3749f28dc4d4ed403f8a4
Joanne <joannemy@my.yorku.ca>
2022-11-14 10:50:29 -0500

    Rounds

--- WH3000.js
+++ WH3000.js
@@ -6,23 +6,44 @@ function submit () {
         document.getElementById("output").innerHTML = "Username already in use, please pick a different username."; 
 }
 
-var timeLeft = 30;
-var elem = document.getElementById('some_div');
-var timerId = setInterval(countdown, 1000);
+function game() {
+    var round = 1;
+    for (i = 0; i < round; i ++) {
+        colourRandom(round)
+
+        var flag = true;
+        var placeHolder = 0;
+            while (flag == true ||  placeHolder < round) {
+                if (buttonSeleted != colour[i])
+                    flag == false;
+                placeHolder++;
+            }
+
+        round++;
+    }
+}
 
 function countdown() {
+var timeLeft = 30;
+var timerId = setInterval(countdown, 1000);
+
     if (timeLeft == -1) {
         clearTimeout(timerId);
         colourRandom();
     } else {
-        elem.innerHTML = timeLeft + ' seconds remaining';
+        document.getElementById("output").innerHTML = timeLeft + ' seconds remaining';
         timeLeft--;
     }
 }
 
-function colourRandom() {
+function colourRandom(round) {
     var colour = ["red", "green", "blue", "black", "white"];
-    var colourChosen = Math.floor(Math.random() * colour.length);
+    var colourChosen = colour[Math.floor(Math.random() * round+1)];
 
     return colourChosen
+}
+
+function colourSelector() {
+    var input = document.getElementById('colourSelection');
+    input.value = colour[0];
 }

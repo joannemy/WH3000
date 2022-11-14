3849a6dc7d1d8de52c174f13d6d87d8a064128c0
Joanne <joannemy@my.yorku.ca>
2022-11-13 23:12:18 -0500

    Timer and colour generator

--- WH3000.js
+++ WH3000.js
@@ -3,6 +3,26 @@ function submit () {
     var unameArr = ["hello"];
 
     if (unameArr.includes(uname)== true)
-        document.getElementById("output").innerHTML = "Username already in use, please pick a different username.";
+        document.getElementById("output").innerHTML = "Username already in use, please pick a different username."; 
+}
 
+var timeLeft = 30;
+var elem = document.getElementById('some_div');
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+        colourRandom();
+    } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+    }
+}
+
+function colourRandom() {
+    var colour = ["red", "green", "blue", "black", "white"];
+    var colourChosen = Math.floor(Math.random() * colour.length);
+
+    return colourChosen
 }

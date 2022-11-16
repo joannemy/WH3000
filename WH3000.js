83c3809994f1f8b2d03ed349c95c73a6a99f0be0
Joanne <joannemy@my.yorku.ca>
2022-11-15 22:49:28 -0500

    main menu

--- WH3000.js
+++ WH3000.js
@@ -1,12 +1,11 @@
-function submit () {
-    var unameArr = ["hello"];
-    flag = false
-
-
-    var uname = document.getElementById("uname").value;
-
-    if (unameArr.includes(uname)== true)
-        document.getElementById("output").innerHTML = "Username already in use, please pick a different username.";
-    else 
-        document.getElementById("output").innerHTML = "Username is valid, please proceed";
+function start() {
+    document.getElementById("instructionButton").style.display = "none";
+    document.getElementById("startButton").style.display = "none";
+    document.getElementById("output").style.display = "none"
 }
+
+function instruction() {
+    document.getElementById("instructionButton").style.display = "none";
+    document.getElementById("output").innerHTML = "For every round a sequence of colours will be temporarily displayed to you once, you will then be prompted to select the colours in the correct order. You have only ONE chance. The number of colours in the sequence is the same as the round number. Ex. if you are on round three the sequence will be 3 colours."
+}
+
